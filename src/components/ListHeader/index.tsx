0ab4b47d889b25431d6b1a45a0879ec67e049639
Manoel Duran <manoel.duran@hotmail.com>
2022-02-08 09:59:03 -0300

    search and listheader create

---
+++ src/components/ListHeader/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {
+Container
+} from './styles';
+
+export function ListHeader(){
+    return (
+        <Container>
+            
+        </Container>
+    );
+}
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,12 +1,49 @@
 import React from 'react';
+import { useTheme } from 'styled-components/native';
+import { MaterialIcons } from '@expo/vector-icons';
+import happyEmoji from '@assets/happy.png';
+import { useAuth } from '@hooks/auth';
+import { Search } from '@components/Search';
+import { ListHeader } from '@components/ListHeader';
 import {
-    Container
+    Container,
+    Header,
+    Greeting,
+    GreetingEmoji,
+    GreetingText,
+    LogOut,
+    MenuHeader,
+    MenuItemsNumber,
+    Title
 } from './styles';
 
+
 export function Home() {
+    const { signOut } = useAuth();
+    const theme = useTheme();
     return (
         <Container>
-            
+            <Header>
+                <Greeting>
+                    <GreetingEmoji source={happyEmoji} />
+                    <GreetingText>Olá, Admin</GreetingText>
+                </Greeting>
+                <LogOut onPress={signOut} >
+                    <MaterialIcons
+                        name='logout'
+                        color={theme.COLORS.TITLE}
+                        size={24}
+                    />
+                </LogOut>
+            </Header>
+            <Search
+                onSearch={() => { }} onClear={() => { }}
+            />
+            <MenuHeader>
+                <Title>Cardápio</Title>
+                <ListHeader />
+                <MenuItemsNumber>321</MenuItemsNumber>
+            </MenuHeader>
         </Container>
     );
 }
